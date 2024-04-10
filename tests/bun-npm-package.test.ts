7cb3817fbdebe3163cf882c36dc352b73c7ac064
rai <mj82xx@gmail.com>
2024-04-10 17:24:56 +0900

    add changeset

--- tests/bun-npm-package.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "vitest";
-import { helloWorld } from "../templates/bun-npm-package/dist/esm";
-
-describe("bun-npm-package", () => {
-  test("should pass esm", () => {
-    expect(helloWorld()).toBe("Hello, world!");
-  });
-});
